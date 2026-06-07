73cb679b5bdf282b64ff0f25b39b9c72ca8eec16
František Radoš <frantisek.rados1@gmail.com>
2026-06-07 16:41:52 +0200

    Add files via upload

---
+++ products_table.sql
@@ -0,0 +1,16 @@
+SELECT * FROM products;
+
+INSERT INTO products (name, price, stock)
+VALUES ('Notebook', 999.99, 10);
+
+INSERT INTO products (name, price, stock)
+VALUES ('Mys', 25.50, 50);
+
+INSERT INTO products (name, prise, stock)
+VALUES ('Klavesnica' 49.90, 25);
+
+SELECT * FROM products;
+
+UPDATE products SET price = 799.99 WHERE name = 'Notebook';
+
+SELECT * FROM products;
